3a8b246f0512a5acb0a4814520a58a561c1d6934
manntrr <man22057@byui.edu>
2023-11-25 20:43:07 -0500

    fix entry and show base values

--- wdd230/chamber/scripts/directory.js
+++ wdd230/chamber/scripts/directory.js
@@ -7,17 +7,38 @@ async function getMemberData() {
             let card = document.createElement('section');
             let fullName = document.createElement('h2');
             let portrait = document.createElement('img');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
+            let webSite = document.createElement('a');
+            let level = document.createElement('p');
+            let hours = document.createElement('ul');
+            let Description = document.createElement('p');
 
             fullName.textContent = `${member.name}`;
-            portrait.setAttribute('src', `images/${member.imageurl}`);
+            card.appendChild(fullName);
+            portrait.setAttribute('src', `images/${member.image}`);
             portrait.setAttribute('alt', `Logo of ${member.name}`);
             portrait.setAttribute('loading', `lazy`);
-            portrait.setAttribute('width', `340`);
-            portrait.setAttribute('height', `440`);
-
-            card.appendChild(fullName);
+            portrait.setAttribute('width', `150`);
+            portrait.setAttribute('height', `90`);
             card.appendChild(portrait);
-
+            address.textContent = member.address;
+            card.appendChild(address);
+            phone.textContent = member.phone;
+            card.appendChild(phone);
+            webSite.textContent = member.website;
+            webSite.setAttribute('href', member.website);
+            webSite.setAttribute('target', '_blank');
+            card.appendChild(webSite);
+            level.textContent = member.membershipLevel;
+            card.appendChild(level);
+            member.hours.forEach(line => {
+                let lineItem = document.createElement('li');
+                lineItem.textContent = line;
+                hours.appendChild(lineItem);
+            });
+            card.appendChild(hours);
+            Description.textContent = member.description;
             cards.appendChild(card);            
         });
     }
