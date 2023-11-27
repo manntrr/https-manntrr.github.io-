82253e1b6f46073018e52a3f87f9b92642980df7
manntrr <man22057@byui.edu>
2023-11-26 22:36:08 -0500

    view control added to directory

--- wdd230/chamber/scripts/directory.js
+++ wdd230/chamber/scripts/directory.js
@@ -1,32 +1,52 @@
 const url = 'https://manntrr.github.io/wdd230/chamber/data/members.json';
 const cards = document.querySelector('#cards');
+var gridActive = true;
 
-/*
-    https://icons8.com/icon/set/list-view/small
-*/
 async function getMemberData() {
-    const displayMembers = (members) => {
-        let control = document.createElement('div');
-        let gridButton = document.createElement('input');
-        let gridImage = document.createElement('img');
-        let listButton = document.createElement('input');
-        let listImage = document.createElement('img');
-        control.setAttribute('class','directoryControl');
-        gridButton.setAttribute('class','directoryControl');
-        gridImage.setAttribute('class','directoryControl');
-        listButton.setAttribute('class','directoryControl');
-        listImage.setAttribute('class','directoryControl');
+    const response = await fetch(url);
+    /*
+        https://icons8.com/icon/set/list-view/small
+    */
+    let control = document.createElement('div');
+    let gridButton = document.createElement('button');
+    let gridImage = document.createElement('img');
+    let listButton = document.createElement('button');
+    let listImage = document.createElement('img');
+    control.setAttribute('class','directoryControl');
+    gridButton.setAttribute('class','directoryGridButton');
+    gridButton.setAttribute('value',gridActive);
+    listButton.setAttribute('class','directoryListButton');
+    listButton.setAttribute('value',!gridActive);
+    if(gridActive) {
+        gridButton.classList.toggle('directoryControlSelected');
+    } else {
+        listButton.classList.toggle('directoryControlSelected');
+    }
+    gridButton.addEventListener('click', () => {
+        gridActive = true;
+        cards.innerHTML="";
+        getMemberData();
+    });
+    listButton.addEventListener('click', () => {
+        gridActive = false;
+        cards.innerHTML="";
+        getMemberData();
+    });
 
-        gridImage.setAttribute('src','images/grid-view-icon.png');
-        gridImage.setAttribute('alt','Grid View Icon');
-        listImage.setAttribute('src','images/list-view-icon.png');
-        listImage.setAttribute('alt','List View Icon');
+    gridImage.setAttribute('class','directoryGridImage');
+    gridImage.setAttribute('src','images/grid-view-icon.png');
+    gridImage.setAttribute('alt','Grid View Icon');
+    listImage.setAttribute('class','directoryListImage');
+    listImage.setAttribute('src','images/list-view-icon.png');
+    listImage.setAttribute('alt','List View Icon');
 
-        gridButton.appendChild(gridImage);
-        listButton.appendChild(listImage);
-        control.appendChild(gridButton);
-        control.appendChild(listButton);
-        cards.appendChild(control);
+    gridButton.appendChild(gridImage);
+    listButton.appendChild(listImage);
+    control.appendChild(gridButton);
+    control.appendChild(listButton);
+    cards.appendChild(control);
+
+    const displayMembersCard = (members) => {
         members.forEach(member => {
             let card = document.createElement('section');
             let fullName = document.createElement('h2');
@@ -38,6 +58,8 @@ async function getMemberData() {
             let hours = document.createElement('ul');
             let Description = document.createElement('p');
 
+            card.setAttribute('class','directoryCard');
+            fullName.setAttribute('class','directoryCardHeading');
             fullName.textContent = `${member.name}`;
             portrait.setAttribute('src', `images/${member.image}`);
             portrait.setAttribute('alt', `Logo of ${member.name}`);
@@ -74,10 +96,57 @@ async function getMemberData() {
             cards.appendChild(card);            
         });
     }
-    const response = await fetch(url);
+
+    const displayMembersList = (members) => {
+        members.forEach(member => {
+            let card = document.createElement('section');
+            let fullName = document.createElement('h2');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
+            let webSite = document.createElement('a');
+            let level = document.createElement('p');
+            let hours = document.createElement('ul');
+            let Description = document.createElement('p');
+
+            card.setAttribute('class','directoryCard');
+            fullName.setAttribute('class','directoryCardHeading');
+            fullName.textContent = `${member.name}`;
+            level.textContent = `membership:  ${member.membershipLevel}`;
+            level.setAttribute('class','directoryCardMemberLevel');
+            address.textContent = member.address;
+            address.setAttribute('class','directoryCardAddress');
+            phone.textContent = member.phone;
+            phone.setAttribute('class','directoryCardPhone');
+            webSite.textContent = member.website;
+            webSite.setAttribute('href', member.website);
+            webSite.setAttribute('target', '_blank');
+            webSite.setAttribute('class', 'directoryCardAnchor');
+            member.hours.forEach(line => {
+                let lineItem = document.createElement('li');
+                lineItem.textContent = line;
+                lineItem.setAttribute('class','nonBulletedLineItem');
+                hours.appendChild(lineItem);
+            });
+            Description.textContent = member.description;
+
+            card.appendChild(fullName);
+            card.appendChild(level);
+            card.appendChild(address);
+            card.appendChild(phone);
+            card.appendChild(webSite);
+            card.appendChild(hours);
+
+            cards.appendChild(card);            
+        });
+    }
+
     if (response.ok) {
         const data = await response.json();
-        displayMembers(data.members);
+        if(gridActive) {
+            displayMembersCard(data.members);
+        } else {
+            displayMembersList(data.members);
+        }
     }
 }
 
